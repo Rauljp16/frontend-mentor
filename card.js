bcbd75073455feebdb5dbde2f920a4ce63a520cc
Rauljp16 <raul_astra16@hotmail.com>
2022-09-04 20:26:33 +0200

    3-9

--- card.js
+++ card.js
@@ -7,7 +7,7 @@ let input_yy = document.getElementById("input_yy");
 let input_cvc = document.getElementById("input_cvc");
 let error = document.getElementById("error");
 let button = document.getElementById("button");
-
+let input_name = document.querySelector(".input_name");
 ///////numero de tarjrta
 
 card_number.addEventListener("keyup", (event) => {
@@ -43,52 +43,35 @@ input_cvc.addEventListener("keyup", (event) => {
   document.querySelector(".cvc").innerHTML = inputText;
 });
 
-////// confirm name
+//// confirm name
 
 button.addEventListener("click", (event) => {
   event.preventDefault();
-  validate_name(cardholder_name.value)
-
+  validate(cardholder_name.value);
 });
 
-function validate_name(cardholder_name) {
+function validate(cardholder_name) {
   let exp_reg = /[a-zA-ZñÑáéíóú\s]{1,50}/g;
-
   if (exp_reg.test(cardholder_name) == true) {
-    console.log("is true");
+    error.style.visibility = "hidden";
   } else {
-    error.style.visibility = "visible",
-      cardholder_name.border = red;
-    console.log("nombre no valido")
+    input_name.style.border = "1px solid red";
+    error.style.visibility = "visible";
   }
 }
 
-////// confirm number
-
-// let error_number = document.getElementById("error_number");
-
-// button.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   validate_form(card_number.value)
-
+////////////////////////////////////////////////////////////
+// window.addEventListener("load", () => {
+//   let form = document.getElementById("form");
+//   form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     validaCampos();
+//   });
+//   let validaCampos = () => {
+//     let valor_name = cardholder_name.value;
+//     let valor_number = cardholder_number.value;
+//     let valor_mm = input_mm.value;
+//     let valor_yy = input_yy.value;
+//     let valor_cvc = input_cvc.value;
+//   };
 // });
-
-// function validate_form(card_number) {
-//   let exp_reg_letters = /([.0-9])*\d/g;
-
-//   if (exp_reg_letters.test(card_number) == true) {
-//     console.log("is true");
-//   } else {
-//     card_number.style.border = "3px solid red"
-//     error_number.style.visibility = "visible";
-//     console.log("numero no valido")
-
-//   }
-// }
-
-
-
-// function validate_form(cardholder_number) {
-//   let exp_reg = /[0-9]+/g;
-//   console.log(exp_reg.test(cardholder_number));
-// }
