f3bb55f7fc0d6c5a8c9d73f51c49c30d28e25d8e
Rauljp16 <raul_astra16@hotmail.com>
2022-09-07 18:18:41 +0200

    7/9/22

--- card.js
+++ card.js
@@ -5,9 +5,21 @@ let cardholder_name = document.getElementById("cardholder_name");
 let input_mm = document.getElementById("input_mm");
 let input_yy = document.getElementById("input_yy");
 let input_cvc = document.getElementById("input_cvc");
+
 let error = document.getElementById("error");
+let error_number = document.getElementById("error_number");
+let error_mm = document.getElementById("error_mm");
+let error_yy = document.getElementById("error_yy");
+let error_cvc = document.getElementById("error_cvc");
+
 let button = document.getElementById("button");
+
 let input_name = document.querySelector(".input_name");
+let input_number = document.querySelector(".input_number");
+let input_Cmm = document.querySelector(".inputMM");
+let input_Cyy = document.querySelector(".inputYY");
+let input_Ccvc = document.querySelector(".inputCVC");
+
 ///////numero de tarjrta
 
 card_number.addEventListener("keyup", (event) => {
@@ -53,25 +65,84 @@ button.addEventListener("click", (event) => {
 function validate(cardholder_name) {
   let exp_reg = /[a-zA-ZñÑáéíóú\s]{1,50}/g;
   if (exp_reg.test(cardholder_name) == true) {
+    input_name.style.border = "1px solid black";
     error.style.visibility = "hidden";
   } else {
     input_name.style.border = "1px solid red";
     error.style.visibility = "visible";
+
   }
 }
 
-////////////////////////////////////////////////////////////
-// window.addEventListener("load", () => {
-//   let form = document.getElementById("form");
-//   form.addEventListener("submit", (event) => {
-//     event.preventDefault();
-//     validaCampos();
-//   });
-//   let validaCampos = () => {
-//     let valor_name = cardholder_name.value;
-//     let valor_number = cardholder_number.value;
-//     let valor_mm = input_mm.value;
-//     let valor_yy = input_yy.value;
-//     let valor_cvc = input_cvc.value;
-//   };
-// });
+///// confirm number/////
+
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  validate_number(cardholder_number.value);
+});
+
+function validate_number(cardholder_number) {
+  let exp_reg = /[^a-z ]\ *([.0-9])*\d/g;
+  if (exp_reg.test(cardholder_number) == true) {
+    input_number.style.border = "1px solid black";
+    error_number.style.visibility = "hidden";
+  } else {
+    input_number.style.border = "1px solid red";
+    error_number.style.visibility = "visible";
+  }
+}
+
+//////confirm mm ////////
+
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  validate_mm(input_mm.value);
+});
+
+function validate_mm(input_mm) {
+  let exp_reg = /[^a-z ]\ *([.0-9])*\d/g;
+  if (exp_reg.test(input_mm) == true) {
+    input_Cmm.style.border = "1px solid black";
+    error_mm.style.visibility = "hidden";
+  } else {
+    input_Cmm.style.border = "1px solid red";
+    error_mm.style.visibility = "visible";
+  }
+}
+
+//////confirm yy ////////
+
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  validate_yy(input_yy.value);
+});
+
+function validate_yy(input_yy) {
+  let exp_reg = /[^a-z ]\ *([.0-9])*\d/g;
+  if (exp_reg.test(input_yy) == true) {
+    input_Cyy.style.border = "1px solid black";
+    error_yy.style.visibility = "hidden";
+  } else {
+    input_Cyy.style.border = "1px solid red";
+    error_yy.style.visibility = "visible";
+  }
+}
+//////confirm cvc ////////
+
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  validate_cvc(input_cvc.value);
+});
+
+function validate_cvc(input_cvc) {
+  let exp_reg = /[^a-z ]\ *([.0-9])*\d/g;
+  if (exp_reg.test(input_cvc) == true) {
+    input_Ccvc.style.border = "1px solid black";
+    error_cvc.style.visibility = "hidden";
+  } else {
+    input_Ccvc.style.border = "1px solid red";
+    error_cvc.style.visibility = "visible";
+  }
+}
+
+////ocultar form///////
